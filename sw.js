65b10f7b24b668c9258fdff4f36835632a4c7ba2
jmcjohn14-tech <jmcjohn14@gmail.com>
2025-08-17 00:20:24 -0700

    Add player files (HTML, manifest, icons)

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = "mp3-player-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  // Network-first for html, cache-first for others
+  if (req.mode === "navigate") {
+    e.respondWith(
+      fetch(req).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(
+      caches.match(req).then(res => res || fetch(req))
+    );
+  }
+});
